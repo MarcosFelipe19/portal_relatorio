9c4afc02f050d35c0a20a52234c3352df7290c31
Marcos Felipe <mmuramota1@gmail.com>
2022-11-28 09:02:59 -0300

    up

--- constrollers/controllerSendEmail.js
+++ constrollers/controllerSendEmail.js
@@ -1,29 +1,35 @@
 const nodemailer = require('nodemailer');
+require("dotenv").config();
 
 const sendEmail = {
-    async enviarEmail(req, res) {
-        let user = "teste@construrapidoreformas.com.br";
-        let pass = "Teste123@";
-        let texto = "Prezado Clente, segue o token e a senha para download dos seus relatório.\n\n" +
-            "Para fazer o download dos seu(s) arquivos acesse o portal de relatórios, acesse com, token: ${req.body.token}, senha: ${req.body.senha}. acesso o portal o link a seguir:\n\n" +
-            "link: relatórios";
+    async enviarEmail(orcamento, token, senha, email, nome_empresa, link) {
+        if (!orcamento || !token || !senha || !email || !nome_empresa) {
+            console.log('teste');
+        }
+        let user = process.env.USER;
+        let pass = process.env.PASS;
+        let texto = "<p>Prezado Clente,<br><br>Seus relatórios referente ao Orçamento: <storng>" + orcamento + "</strong> estão  disponíveis para fazer o download.<br><br>" +
+            "Acesse o nosso portal de relatório no endereço: https://www.google.com/ e entre com os seguintes<br>dados abaixo: <br><br> <strong>token: </strong>" + token +
+            "<br><br><strong>senha: </strong>" + senha + "<br><br>Atenciosamente<br><br>Laboratório Lab System<p>";
 
         const transporter = nodemailer.createTransport({
-            host: "smtp.hostinger.com",
-            port: 465,
+            host: process.env.HOST,
+            port: process.env.PORT_MAIL,
             auth: { user, pass }
         })
 
-        transporter.sendMail({
-            from: user,
-            to: req.body.email,
-            subject: `Cliente: ${req.body.nome_empresa} Orçamento: ${req.body.orcamento}`,
-            text: texto,
-        }).then(info => {
-            return res.json({ "msg": "Sucesso!" })
-        }).catch(err => {
-            return res.status(400).json({ "msg": "Error" })
-        })
+        try {
+            await transporter.sendMail({
+                from: user,
+                to: email,
+                subject: `Cliente: ${nome_empresa} Orçamento: ${orcamento}`,
+                html: texto,
+            })
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
     }
 }
 
