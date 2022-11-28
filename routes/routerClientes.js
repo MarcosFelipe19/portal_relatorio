69ba5204d8278dae179da6a7a322f239ece770ee
Marcos Felipe <mmuramota1@gmail.com>
2022-11-28 08:05:32 -0300

    update

--- routes/routerClientes.js
+++ routes/routerClientes.js
@@ -5,6 +5,8 @@ const controllerClientes = require("../constrollers/controllersClientes");
 Router.post("/novoEmail", controllerClientes.novoEmail);
 Router.get("/get", controllerClientes.getCliente);
 Router.get("/getEmails", controllerClientes.getEmails);
+Router.delete("/delete", controllerClientes.deletarEmail);
+Router.put("/update", controllerClientes.updateEmail)
 
 
 module.exports = Router;
